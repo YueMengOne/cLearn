5b0f1bc172bb14b669d94e2db5deeea3e7bb56b3
YueMeng <woyuguohui@outlook.com>
2026-05-17 07:55:55 +0800

    Up

---
+++ 01_Calculate/02_Expression.c
@@ -0,0 +1,13 @@
+//
+// Created by woyug on 2026/5/13.
+//
+
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    printf("c = %d", c);
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("中文输出\n");
+    return 0;
+}
