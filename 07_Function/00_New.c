d43f86bef697fbea9951bb45dcee9e8bbb1e8cd1
YueMeng <woyuguohui@outlook.com>
2026-05-17 11:01:29 +0800

    Up

---
+++ 07_Function/00_New.c
@@ -0,0 +1,31 @@
+//
+// Created by woyug on 2026/5/17.
+//
+
+#include <stdio.h>
+#define NAME "GGIGATHINK, INC."
+#define ADDRESS "101 Magabuck Plaza"
+#define PLACE "Megapolis, CA 94904"
+#define WIDTH 40
+
+// 函数原型
+void starbar(void);
+
+int mian(void){
+    starbar();
+    printf("%s\n", NAME);
+    printf("%s\n", ADDRESS);
+    printf("%s\n", PLACE);
+
+    // 使用函数
+    starbar();
+
+    return 0;
+}
+
+void starbar(void) {
+    int count;
+    for (count = 1; count <= WIDTH; count++)
+        putchar('*');
+    putchar('\n');
+}
--- main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("中文输出\n");
-    return 0;
-}
